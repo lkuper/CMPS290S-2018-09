71e78c6a134ceacddd1919214dd7b0c84fd51be1
Lindsey Kuper <lindsey@composition.al>
2018-09-19 10:43:45 -0700

    Have markdown-to-slides watch for updates.

--- make-slides.sh
+++ make-slides.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-markdown-to-slides -d course-overview.md -o course-overview-slides.html
-open course-overview-slides.html
+markdown-to-slides -d course-overview.md -o course-overview-slides.html --watch & open course-overview-slides.html
