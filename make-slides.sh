3d0f27c8da76ba17aa32cb0ea031bef2634556fe
Lindsey Kuper <lindsey@composition.al>
2018-09-20 14:28:01 -0700

    Tweak the slide-making script to account for new front matter.

--- make-slides.sh
+++ make-slides.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-markdown-to-slides -d course-overview.md -o course-overview-slides.html --level 5 --watch & open course-overview-slides.html
+# Remove Jekyll front matter
+sed -e '1,5d' < course-overview.md > course-overview-slides.md
+
+# Insert presentation title
+sed -i '' '1i\
+# CMPS290S: Languages and Abstractions for Distributed Programming' course-overview-slides.md
+
+# Convert to remark.js slides
+markdown-to-slides -d course-overview-slides.md -o course-overview-slides.html --level 5 --watch & open course-overview-slides.html
+
