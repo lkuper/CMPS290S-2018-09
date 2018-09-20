2518aa165089bfe92b501b8692bb25f0a5869cda
Lindsey Kuper <lindsey@composition.al>
2018-09-20 12:44:00 -0700

    Have markdown-to-slides split at heading level 4.
    (It seems off-by-one?)

--- make-slides.sh
+++ make-slides.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-markdown-to-slides -d course-overview.md -o course-overview-slides.html --watch & open course-overview-slides.html
+markdown-to-slides -d course-overview.md -o course-overview-slides.html --level 5 --watch & open course-overview-slides.html
