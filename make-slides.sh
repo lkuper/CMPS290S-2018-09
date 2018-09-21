708775b0a5dd202e3fd7dbd59309821b98ba3767
Lindsey Kuper <lindsey@composition.al>
2018-09-20 19:49:52 -0700

    This "watch" thing isn't working out so well.

--- make-slides.sh
+++ make-slides.sh
@@ -8,5 +8,8 @@ sed -i '' '1i\
 # CMPS290S: Languages and Abstractions for Distributed Programming' course-overview-slides.md
 
 # Convert to remark.js slides
-markdown-to-slides -d course-overview-slides.md -o course-overview-slides.html --level 5 --watch & open course-overview-slides.html
+markdown-to-slides -d course-overview-slides.md -o course-overview-slides.html --level 5
+
+# Open in browser
+open course-overview-slides.html
 
